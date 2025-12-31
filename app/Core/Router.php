f953e8a85eeb36c01f7db4425c17fad2bc52a366
Eduardo Nacimiento-García <enacimie@ull.edu.es>
2025-12-31 21:56:24 +0000

    Cleanup: Remove debug code

--- app/Core/Router.php
+++ app/Core/Router.php
@@ -27,6 +27,7 @@ class Router
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         $uri = rtrim($uri, '/');
         if ($uri === '') $uri = '/';
+        if ($uri === '') $uri = '/';
 
         $method = $_SERVER['REQUEST_METHOD'];
 
